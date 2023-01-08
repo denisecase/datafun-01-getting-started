72113074a86276dc1eef3ea3c531200c9a66af91
Denise Case <denisecase@users.noreply.github.com>
2023-01-08 07:27:00 -0600

    Add bonus option

---
+++ xtra_p1.py
@@ -0,0 +1,56 @@
+import random
+
+# Change the name below to a name of your choice
+
+name = "GameBot"
+
+# Fix the code below to print the name using an f-string
+
+print()
+print("Hello, I'm {name}, your gamebot.")
+print("Let's play an animal guessing game!")
+print("There are 3 animals: wolf, eagle, snake (a Python of course).")
+print("The wolf scares the eagle.")
+print("The eagle grabs the snake.")
+print("The snake bites the wolf.")
+print("I'll pick one and you pick one and we'll see who wins.")
+print()
+
+# Right now, the user choses wolf everytime.
+# Modify the code so the user is asked to
+# enter wolf, eagle, or snake.
+# Hint: use the input() function
+
+user_choice = "wolf"
+
+# Now the bot will pick one
+buddy_choice = random.choice(["wolf", "eagle", "snake"])
+
+# Report the choices
+print()
+print(f"You said {user_choice}.")
+print(f"I said {buddy_choice}.")
+print()
+
+
+# Now we need to compare the choices and determine the winner
+# Complete the logic to
+# compare the choices and print who won
+# In Python, indentation is important!
+if user_choice == buddy_choice:
+    print("We tied!")
+
+
+# When you finish,
+# right-click on the code and select "Format Document"
+
+# Run the code, and play the game a few times.
+# Copy the output from the terminal and paste it into the
+# docstring comment below.
+# --------------------------------------------------------------------
+"""
+
+
+
+
+"""
